379a88695a33d012cef868acc704d6e59d878ceb
Pedro Correia Cravo Pimenta <pedroccpimenta@gmail.com>
2023-08-19 17:37:57 +0100

    Update simple-interest.sh
    Updating simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# (https://github.com/pedroccpimenta)[Pedro Pimenta]
+# pedroccpimenta - https://github.com/pedroccpimenta
 # Input:
 # p, principal amount
 # t, time period in years
